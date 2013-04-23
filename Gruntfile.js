99162df94eb1cef47875a2236fba2ff92ae6cb1e
designeng <denissavenok@gmail.com>
2013-04-23 20:25:18 +0400

    initial project version

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  // Project configuration.
+  grunt.initConfig({
+    jasmine : {
+      src : 'src/**/*.js',
+      options : {
+        specs : 'spec/**/*.js',
+        template : require('grunt-template-jasmine-istanbul'),
+        templateOptions: {
+          coverage: 'reports/coverage.json',
+          report: 'reports/coverage'
+        }
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+  grunt.registerTask('default', ['jasmine']);
+};
